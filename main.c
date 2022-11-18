dd812a762b19bf3d272b8836ffd0da99a4d26ef3
Riza <yukiluizamaria@gmail.com>
2022-11-18 16:36:26 -0300

    Tentando criar usuário, ainda está com um erro pois na hora do loop(que na minha opinião eu iria fazer uma função no lugar do ->eu<-  para checar se já tem algum usuário com esse nome) porém parece que não consegue identificar aquele ->eu<-

--- main.c
+++ main.c
@@ -1,1 +1,57 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
+/*
+FILE *CriaArquivo(char s[200])
+{
+FILE *p;
+p = fopen(s, "wb");
+return(p);
+}
+*/
+
+typedef struct {
+char* user;
+char* password;
+} tUsuario, *tPtrUsuario;
+
+int main()
+{
+    char quest_cadastro;
+    tUsuario Usuario;
+    tPtrUsuario PtrUsuario;
+
+    do
+    {
+        printf("Digite se você já tem cadastro(s/n)\n");
+        printf("     (Digite 1 para sair)\n");
+        printf("------> ");
+        scanf(" %c", &quest_cadastro);
+        quest_cadastro = tolower(quest_cadastro);
+    }while(quest_cadastro != 's' && quest_cadastro != 'n' && quest_cadastro != '1');
+
+    if (quest_cadastro == 'n')
+    {
+        FILE* cadastro;
+
+        cadastro = fopen("login_usuario.txt","w");
+
+        do{
+        printf("\nDigite o seu Usuário: ");
+        scanf("%s", PtrUsuario->user);
+        printf("\n%s", PtrUsuario->user);
+        }while(PtrUsuario->user == "eu"); //Erro aqui
+
+        fclose(cadastro);
+    }
+
+    else if (quest_cadastro == 's')
+    {
+        printf("sim");
+    }
+
+    printf("\nSaindo do Programa...");
+
+    return 0;
+}
